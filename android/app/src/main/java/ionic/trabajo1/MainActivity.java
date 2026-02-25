95dd86ce0be7c282722841137e65fe968a99d365
NicoleNohemiEspa <nespana@estud.tesa.edu.ec>
2026-02-24 18:50:59 -0600

    first commit

---
+++ android/app/src/main/java/ionic/trabajo1/MainActivity.java
@@ -0,0 +1,5 @@
+package ionic.trabajo1;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ionic.trabajo1',
+  appName: 'trabajo1',
+  webDir: 'dist'
+};
+
+export default config;
